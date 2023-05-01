607de979113d3e0b1ea099f4fce44ef19d5fa9d0
Utkarsh Vishwakarma <utkarsh@Utkarshs-MacBook-Air.local>
2023-05-01 17:28:39 +0530

    Initial Fuzz test file

--- Tests/fuzz_test.py
+++ Tests/fuzz_test.py
@@ -1,5 +1,7 @@
 import string
-from hypothesis import settings, strategies as st, given
+
+from hypothesis import given, settings
+from hypothesis import strategies as st
 import dns.resolver
 from spooflib.spoof_checker import SPFChecker, DMARCChecker
 
@@ -9,9 +11,9 @@ from spooflib.spoof_checker import SPFChecker, DMARCChecker
 
 def domain_name_strategy() -> st.SearchStrategy[str]:
     tlds = [".com", ".org", ".net", ".io", ".in"]
-    label = st.text(alphabet=string.ascii_letters + string.digits + "-", min_size=1, max_size=63).filter(
-        lambda x: x[0] != '-' and x[-1] != '-'
-    )
+    label = st.text(
+        alphabet=string.ascii_letters + string.digits + "-", min_size=1, max_size=63
+    ).filter(lambda x: x[0] != '-' and x[-1] != '-')
     domain = st.lists(label, min_size=1, max_size=5).map(".".join)
     return st.tuples(domain, st.sampled_from(tlds)).map(lambda x: x[0] + x[1])
 
@@ -60,5 +62,5 @@ def test_dmarc_checker(domain):
 
 if __name__ == "__main__":
     for _ in range(5):
-        test_spf_checker()
+        # test_spf_checker()
         test_dmarc_checker()
